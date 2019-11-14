a37ddd27bcc64f043815c7a8301a1da8c7490ae5
sarsij <sarsij@skeps.com>
2019-11-14 14:21:14 +0530

    update
    update

--- src/app.js
+++ src/app.js
@@ -7,8 +7,15 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const accountsData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const usersData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+
+const accounts = JSON.parse(accountsData);
+const users = JSON.parse(usersData);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+// app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
 
 app.listen(3000, () => console.log('PS Project running on port 3000!'));
